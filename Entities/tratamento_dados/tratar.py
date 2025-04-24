45615d11177e572fca281f451733572d0b62368d
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2025-04-24 11:02:07 -0300

    corrigindo sheet

--- Entities/tratamento_dados/tratar.py
+++ Entities/tratamento_dados/tratar.py
@@ -13,7 +13,7 @@ multiprocessing.freeze_support()
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 # Lista de sheets válidas que podem ser processadas
-valid_sheets:List[str] = ['Sheet1']
+valid_sheets:List[str] = ['Base de Dados']
 
 # Contador auxiliar para possíveis usos futuros
 count:int = 0
